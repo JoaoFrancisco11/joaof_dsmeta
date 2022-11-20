4de241b3e1255ddf1e9f0b3d6d49dde1b8f188f8
Joao Francisco <joaofrancisco.monteiro11@gmail.com>
2022-11-19 23:12:43 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="DSMeta" />
+                <h1>DSMeta</h1>
+                <p>
+                    Desenvolvido por
+                    <a href="https://github.com/JoaoFrancisco11"> João Francisco </a>
+                    durante a semana de Spring React do programa DevSuperior.
+                </p>
+            </div>
+        </header>
+    )
+
+}
+
+export default Header
